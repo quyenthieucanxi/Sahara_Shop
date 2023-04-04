7602ab972a1df632f1f32d0ca8ceff12d9eaf9f4
Huỳnh Công Quyền <congquyenhuynh111@gmail.com>
2023-04-05 01:37:36 +0700

    Add Layout

---
+++ app/src/main/java/com/example/saharashop/activity/SignupActivity.java
@@ -0,0 +1,18 @@
+package com.example.saharashop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.saharashop.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+
+}
