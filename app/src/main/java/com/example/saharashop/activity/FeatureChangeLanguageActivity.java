faab12c954f299dfde876bedcc52dc6ed03f9ed6
Huỳnh Công Quyền <congquyenhuynh111@gmail.com>
2023-05-11 02:54:33 +0700

    handle Search Product

--- app/src/main/java/com/example/saharashop/activity/FeatureChangeLanguageActivity.java
+++ app/src/main/java/com/example/saharashop/activity/FeatureChangeLanguageActivity.java
@@ -1,10 +1,13 @@
 package com.example.saharashop.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.saharashop.R;
+import com.example.saharashop.fragment.MessageBoxFragment;
 
 public class FeatureChangeLanguageActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class FeatureChangeLanguageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature_change_language);
+        findViewById(R.id.btnBack_ChangeLanguage).setOnClickListener(v -> {
+            finish();
+        });
+    }
+    public void chooseLanguage(View view) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        MessageBoxFragment msg = new MessageBoxFragment();
+        msg.show(transaction, "OKe");
     }
 }
--- app/src/main/java/com/example/saharashop/api/IProductType.java
+++ app/src/main/java/com/example/saharashop/api/IProductType.java
@@ -24,4 +24,10 @@ public interface IProductType {
     @POST("product/getProductByListTypeId")
     Call<List<Product>> getProductByListTypeId(@Body Map<String, ArrayList<String>> requestBody);
 
+    @POST("product/getProductByTypeName")
+    Call<List<Product>> getProductByTypeName(@Body Map<String,String> TxtSearch);
+
+    @GET("product/getAllProducts")
+    Call<List<Product>> getAllProducts();
+
 }
